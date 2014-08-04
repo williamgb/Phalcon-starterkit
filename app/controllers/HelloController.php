eb34de0d29880dcb566c3777bc69c6c0baf06039
William Grant-Buckley <ahoy@williambuckley.co.uk>
2014-08-04 18:05:43 +0100

    Replace tabs with four 4 spaces

--- app/controllers/HelloController.php
+++ app/controllers/HelloController.php
@@ -4,10 +4,10 @@
  *
  * A simple starterKit for PhalconPHP. 
  *
- * @package		StarterKit
- * @author		Jeremie Ges & Laurent Schaffner
- * @link		https://github.com/GesJeremie/PhalconPHP-StarterKit
- * @since		Version 0.1
+ * @package        StarterKit
+ * @author         Jeremie Ges & Laurent Schaffner
+ * @link           https://github.com/GesJeremie/PhalconPHP-StarterKit
+ * @since          Version 0.1
  */
 
 // ------------------------------------------------------------------------
@@ -17,24 +17,24 @@
  *
  * A simple example of controller system.
  *
- * @package		PhalconPHP
- * @subpackage	Controllers
- * @category	Controllers
+ * @package     PhalconPHP
+ * @subpackage  Controllers
+ * @category    Controllers
  */
 
 class HelloController extends ControllerBase
 {
-	/**
-	 * Index
-	 *
-	 * Just render the home view (view/hello/index)
-	 * This method use the auto-view system of Phalcon.
-	 * 
-	 * @link https://phalcon-php-framework-documentation.readthedocs.org/en/latest/reference/views.html
-	 *
-	 * @access	public
-	 * @return	void
-	 */
+    /**
+     * Index
+     *
+     * Just render the home view (view/hello/index)
+     * This method use the auto-view system of Phalcon.
+     * 
+     * @link https://phalcon-php-framework-documentation.readthedocs.org/en/latest/reference/views.html
+     *
+     * @access	public
+     * @return	void
+     */
     public function indexAction()
     {
 
